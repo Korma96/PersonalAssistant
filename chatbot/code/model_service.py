7355c01a368d53ded38ce05c8b1af6f5ed6ddb64
Marko Radovic <markoradovic996@gmail.com>
2020-02-22 08:17:27 +0100

    set relu as activation function

--- chatbot/code/model_service.py
+++ chatbot/code/model_service.py
@@ -7,8 +7,8 @@ def create_model(train_x_data_shape, train_y_data_shape):
     tf.reset_default_graph()
     # Build neural network
     net = tflearn.input_data(shape=train_x_data_shape)
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8, activation="relu")
+    net = tflearn.fully_connected(net, 8, activation="relu")
     net = tflearn.fully_connected(net, train_y_data_shape, activation='softmax')
     net = tflearn.regression(net)
 
